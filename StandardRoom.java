51375e19e0658accc20e3581854223765a1e6c75
viphoangdep <131168301+viphoangdep@users.noreply.github.com>
2024-04-27 15:29:18 +0700

    no contain cc file

---
+++ StandardRoom.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class StandardRoom extends Room {
+    private boolean havingShower ;
+    
+    
+    public StandardRoom(int room_id, double price, String check_in_date, int numOfDay, int numOfBed,
+            boolean isAvailable, String type, List<Service> bookedService, boolean havingShower) {
+        super(room_id, price, check_in_date, numOfDay, numOfBed, isAvailable, type, bookedService);
+        this.havingShower = havingShower;
+    }
+
+    public StandardRoom(boolean havingShower) {
+        this.havingShower = havingShower;
+    }
+
+    public boolean isHavingShower() {
+        return havingShower;
+    }
+
+    public void setHavingShower(boolean havingShower) {
+        this.havingShower = havingShower;
+    };
+
+    @Override
+    public double calculatePrice(){
+        int showerPrice = this.isHavingShower() ? 50 : 0;
+        return this.getNumOfDay() * (this.getPrice() + this.getNumOfBed() * 50 + showerPrice);
+    }
+
+    
+}
