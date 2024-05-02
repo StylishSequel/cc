45f5a7cb0bcfb91b43234e79f355edb7161a41c6
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-02 21:36:33 +0700

    Fix e Customer.java

--- src/Person/Customer.java
+++ src/Person/Customer.java
@@ -1,4 +1,5 @@
 package Person;
+
 import ConnectDatabase.ConnectDatabase;
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -12,43 +13,47 @@ import Service.Service;
 public class Customer extends Person {
     private List<Room> bookedRoom;
 
-
-    public Customer(int ID, String name, boolean gender, String phone){
-        super(ID, name, gender, phone);
+    public Customer(int ID, String name, boolean gender, String phone, boolean is_active) {
+        super(ID, name, gender, phone, is_active);
         bookedRoom = new ArrayList<>();
     }
-    public List<Room> getBookedRoom(){
+
+    public Customer(String name, boolean gender, String phone, boolean is_active) {
+        super(name, gender, phone, is_active);
+        bookedRoom = new ArrayList<>();
+    }
+
+    public List<Room> getBookedRoom() {
         return bookedRoom;
     }
 
-    
+    // public void bookService(Hotel h){
+    // System.out.println("Enter service id: ");
+    // Scanner sc = new Scanner(System.in);
+    // int id = sc.nextInt();
+    // List<Service> services = h.getServices();
+    // for(Service service : services){
+    // if(service.getId() == id){
+    // Services.add(service);
+    // System.out.println("Service booked successfully");
+    // }
+    // else {
+    // System.out.println("Service is not found");
+    // }
+    // }
+    // }
 
-//    public void bookService(Hotel h){
-//        System.out.println("Enter service id: ");
-//        Scanner sc = new Scanner(System.in);
-//        int id = sc.nextInt();
-//        List<Service> services = h.getServices();
-//        for(Service service : services){
-//            if(service.getId() == id){
-//                Services.add(service);
-//                System.out.println("Service booked successfully");
-//            }
-//            else {
-//                System.out.println("Service is not found");
-//            }
-//        }
-//    }
 
-    public void printBookedRoom(){
-        for(Room room : bookedRoom){
+    public void printBookedRoom() {
+        for (Room room : bookedRoom) {
             System.out.println(room);
         }
     }
 
-    public void printServices(){
-        for(Room room : bookedRoom){
+    public void printServices() {
+        for (Room room : bookedRoom) {
             List<Service> s = room.getBookedService();
-            for(Service service : s){
+            for (Service service : s) {
                 System.out.println(service.toString());
             }
         }
@@ -59,7 +64,7 @@ public class Customer extends Person {
         for (Room room : bookedRoom) {
             total += room.getPrice();
             List<Service> s = room.getBookedService();
-            for(Service service : s){
+            for (Service service : s) {
                 total += service.getPrice();
             }
         }
@@ -110,17 +115,13 @@ public class Customer extends Person {
 
         
             // ID chỉ có 1 nên không cần dùng for
-    
-            // Room r = rooms.stream().filter(room -> room.getId() == id).findFirst().orElse(null);
-            // if (r == null) {
-            //     System.out.println("Room is not found");
-            //     return;
-            // }
-            // r.setAvailable(false);
-            // System.out.println("Room booked successfully");
-            // bookedRoom.add(r);
-        
+            
+            
         sc.close();
     }
 }
+
+    
+
+   
 
