f1430f6725139b7ae03e29ee2e3aca8ef685159b
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-02 21:33:22 +0700

    Update is_active Person.java

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -5,34 +5,51 @@ public class Person {
     private String name;
     private boolean gender;
     private String phone;
+    private boolean is_active;
 
-    public Person(int ID, String name, boolean gender, String phone) {
+    public Person(int ID, String name, boolean gender, String phone, boolean is_active) {
         this.ID = ID;
         this.name = name;
         this.gender = gender;
         this.phone = phone;
+        this.is_active = is_active;
     }
+
+    public Person(String name, boolean gender, String phone, boolean is_active) {
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+        this.is_active = is_active;
+    }
+
     public int getID() {
         return ID;
     }
+
     public String getName() {
         return name;
     }
+
     public boolean isGender() {
         return gender;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setID(int ID) {
         this.ID = ID;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setGender(boolean gender) {
         this.gender = gender;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
