b1eb1b2364d8e0f257fa732aa8a4f50ee3237e39
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-13 17:18:28 +0700

    Update IQuery.java

--- src/ConnectDatabase/IQuery.java
+++ src/ConnectDatabase/IQuery.java
@@ -1,5 +1,15 @@
 package ConnectDatabase;
 
-public interface IRepository {
+import java.util.List;
 
+public interface IQuery<T> {
+    public void insert(T t);
+
+    public void update(T t);
+
+    public void delete(T t);
+
+    public T select(int id);
+
+    public List<T> selectAll();
 }
