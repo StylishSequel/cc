141b8057799c796233f2ceb97dfb4734f52243a5
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-13 17:18:42 +0700

    Update Connector.java

--- src/ConnectDatabase/Connector.java
+++ src/ConnectDatabase/Connector.java
@@ -1,5 +1,27 @@
 package ConnectDatabase;
 
-public class Connector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Connector {
+    private String hostName = "localhost:5432";
+    private String databaseName = "test";
+    private String username = "postgres";
+    private String password = "123456";
+
+    private String connectionURL = "jdbc:postgresql://" + hostName + "/" + databaseName;
+
+    public Connection connect() {
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection(connectionURL, username, password);
+            System.out.println("Connected to database");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return con;
+    }
 }
