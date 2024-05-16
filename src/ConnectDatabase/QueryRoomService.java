3411cc7a607ba3858cb0736ca4d8a3080aa8bf1d
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-16 22:12:03 +0700

    Update QueryRoomService.java

--- src/ConnectDatabase/QueryRoomService.java
+++ src/ConnectDatabase/QueryRoomService.java
@@ -33,7 +33,8 @@ public class QueryRoomService {
                 "JOIN room_services rs ON r.room_id = rs.room_id" +
                 "JOIN customer_rooms cr ON r.room_id = cr.room_id" +
                 "JOIN services s ON rs.service_id = s.service_id" +
-                "WHERE check_out_date IS NULL AND r.room_id = ?;";
+                "WHERE check_out_date IS NULL AND r.room_id = ?;"+
+                "AND date > check_in_date";
         List<Service> services = new ArrayList<>();
         try (Connection con = connector.connect();
                 PreparedStatement pstmt = con.prepareStatement(query)) {
@@ -46,5 +47,52 @@ public class QueryRoomService {
             throw new RuntimeException(e);
         }
         return services;
+    }
+
+    public double calculateRoomService(int roomId) {
+        String query = "SELECT SUM(s.price) AS total " +
+        "FROM rooms r " +
+        "JOIN customer_rooms cr ON r.room_id = cr.room_id " +
+        "JOIN room_services rs ON r.room_id = rs.room_id " +
+        "JOIN services s ON rs.service_id = s.service_id " +
+        "WHERE cr.check_out_date IS NULL AND date > check_in_date AND r.room_id = ?";
+    
+        try (Connection con = connector.connect();
+                PreparedStatement pstmt = con.prepareStatement(query)) {
+            pstmt.setInt(1, roomId);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getDouble("total");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    
+        return 0.0;
+    }
+
+    public List<Double> calculateRoomServiceTotals(int customerId) {
+        String query = "SELECT SUM(s.price) AS total " +
+        "FROM rooms r " +
+        "JOIN customer_rooms cr ON r.room_id = cr.room_id " +
+        "JOIN room_services rs ON r.room_id = rs.room_id " +
+        "JOIN services s ON rs.service_id = s.service_id " +
+        "WHERE cr.check_out_date IS NULL AND date > check_in_date " +
+        "GROUP BY r.room_id" +
+        "WHERE cr.customer_id = ?";
+    
+        List<Double> totals = new ArrayList<>();
+    
+        try (Connection con = connector.connect();
+                PreparedStatement pstmt = con.prepareStatement(query)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                totals.add(rs.getDouble("total"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    
+        return totals;
     }
 }
