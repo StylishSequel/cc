7c53c3cd2051c519384dd1cb602ca74beca7c393
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-16 22:07:19 +0700

    add db

--- src/Hotel/Hotel.java
+++ src/Hotel/Hotel.java
@@ -2,7 +2,8 @@ package Hotel;
 
 import Person.Customer;
 import Person.Employee;
-import Room.*;
+import Room.Room;
+import Room.StandardRoom;
 import Service.Service;
 
 import java.util.ArrayList;
@@ -10,27 +11,23 @@ import java.util.List;
 import java.util.Scanner;
 
 import ConnectDatabase.ConnectDatabase;
+import ConnectDatabase.IQuery;
 
 public class Hotel {
     private String name;
     private String address;
-    List<Room> rooms;
-    List<Customer> customers;
-    List<Employee> employees;
-    List<Service> services;
-    List<Service> tasks;
-
-    public Hotel(String name, String address) {
-        ConnectDatabase connector = new ConnectDatabase();
-        
+    IQuery<Customer> customerQuery;
+    IQuery<Employee> employeeQuery;
+    IQuery<Room> roomQuery;
+    IQuery<Service> serviceQuery;
+    
+    public Hotel(String name, String address, IQuery<Customer> customerQuery, IQuery<Employee> employeeQuery, IQuery<Room> roomQuery, IQuery<Service> serviceQuery) {
         this.name = name;
         this.address = address;
-        rooms = connector.queryRooms();
-        rooms.add(new StandardRoom(true));
-        customers = connector.queryCustomers();
-        employees = connector.queryEmployees();
-        services = connector.queryAllServices();
-        tasks = new ArrayList<>();
+        this.customerQuery = customerQuery;
+        this.employeeQuery = employeeQuery;
+        this.roomQuery = roomQuery;
+        this.serviceQuery = serviceQuery;
     }
 
     public String getName() {
@@ -41,28 +38,28 @@ public class Hotel {
         return this.address;
     }
 
-    public List<Customer> getCustomers() {
-        return this.customers;
-    }
+    // public List<Customer> getCustomers() {
+    //     return customerQuery.selectAll();
+    // }
 
-    public List<Employee> getEmployees() {
-        return this.employees;
-    }
+    // public List<Employee> getEmployees() {
+    //     return employeeQuery.selectAll();
+    // }
 
-    public void printEmployee() {
-        for (Employee employee : employees) {
-            System.out.println(employee.toString());
-        }
-    }
+    // public void printEmployee() {
+    //     for (Employee employee : employeeQuery.selectAll()) {
+    //         System.out.println(employee.toString());
+    //     }
+    // }
 
-    public List<Service> getServices() {
-        return this.services;
-    }
+    // public List<Service> getServices() {
+    //     return serviceQuery.selectAll();
+    // }
 
-    public void printRooms() {
-        System.out.println("Rooms in hotel:");
-        rooms.forEach(r -> System.out.println(r.toString()));
-    }
+    // public void printRooms() {
+    //     System.out.println("Rooms in hotel:");
+    //     rooms.forEach(r -> System.out.println(r.toString()));
+    // }
 
     public void addCustomer() {
         // Add customer
@@ -79,13 +76,9 @@ public class Hotel {
         Boolean is_active = scan.nextBoolean();
         ConnectDatabase connector = new ConnectDatabase();
         Customer customer = connector.insertCustomer(name, gender, phone, is_active);
-        customers.add(customer);
-        scan.close();
+        customerQuery.insert(customer);
     }
 
-    public void addRoom(Room room) {
-        rooms.add(room);
-    }
 
     public void addRoom() {
         Scanner scan = new Scanner(System.in);
@@ -107,103 +100,86 @@ public class Hotel {
                 int shower = scan.nextInt();
                 scan.nextLine();
                 StandardRoom newSR = new StandardRoom(price, beds, shower == 1);
-                this.addRoom(newSR);
+                roomQuery.insert(newSR);
                 break;
 
             case 2:
                 System.out.println("Enter furniture:");
                 String furniture = scan.nextLine();
                 DeluxeRoom newDR = new DeluxeRoom(price, beds, furniture);
-                this.addRoom(newDR);
+                roomQuery.insert(newDR);
                 break;
 
             case 3:
                 System.out.println("Enter electric devices:");
                 String devices = scan.nextLine();
-                SuiteRoom newSDR = new SuiteRoom(price, beds, devices);
-                this.addRoom(newSDR);
+                SuiteRoom newSR = new SuiteRoom(price, beds, devices);
+                roomQuery.insert(newSR);
                 break;
 
             default:
                 System.out.println("Invalid choice");
                 break;
         }
-        scan.close();
     }
 
     public void removeRoom() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter room id to remove: ");
         int id = scan.nextInt();
-        Room room = rooms.stream().filter(r -> r.getId() == id).findFirst().orElse(null);
-        if (room == null) {
-            System.out.println("Room not found");
-            scan.close();
-            return;
-        }
-        rooms.remove(room);
+        roomQuery.delete(roomQuery.select(id));
         System.out.println("Room removed successfully");
-        scan.close();
+
     }
 
-    public void removeRoom(Room room) {
-        rooms.remove(room);
-    }
+    // public void checkOut() {
+    //     Scanner scan = new Scanner(System.in);
+    //     System.out.println("Enter customer id: ");
+    //     int id = scan.nextInt();
+    //     Customer customer = customers.stream().filter(c -> c.getID() == id).findFirst().orElse(null);
+    //     if (customer == null) {
+    //         System.out.println("Customer not found");
+    //         return;
+    //     }
+    //     customer.getBookedRoom().stream().forEach(r -> r.toString());
+    //     System.out.println("Enter room id to check out: ");
+    //     int roomId = scan.nextInt();
+    //     scan.nextLine();
+    //     Room room = customer.getBookedRoom().stream().filter(r -> r.getId() == roomId).findFirst().orElse(null);
+    //     if (room == null) {
+    //         System.out.println("Room not found");
+    //         return;
+    //     }
+    //     room.setAvailable(true);
+    //     ConnectDatabase connector = new ConnectDatabase();
+    //     connector.updateAvailableRoom(roomId);
 
-    public void checkOut() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter customer id: ");
-        int id = scan.nextInt();
-        Customer customer = customers.stream().filter(c -> c.getID() == id).findFirst().orElse(null);
-        if (customer == null) {
-            System.out.println("Customer not found");
-            return;
-        }
-        customer.getBookedRoom().stream().forEach(r -> r.toString());
-        System.out.println("Enter room id to check out: ");
-        int roomId = scan.nextInt();
-        scan.nextLine();
-        Room room = customer.getBookedRoom().stream().filter(r -> r.getId() == roomId).findFirst().orElse(null);
-        if (room == null) {
-            System.out.println("Room not found");
-            return;
-        }
-        room.setAvailable(true);
-        ConnectDatabase connector = new ConnectDatabase();
-        connector.updateAvailableRoom(roomId);
-
-        if(customer.getBookedRoom().size() == 0){
-            connector.updateActiveCustomer(customer.getID());
-            customers.remove(customer);
-        }
-        System.out.println("Room checked out successfully");
-    }
-
-    public void checkOut(Customer customer) {
-        customer.getBookedRoom().stream().forEach(r -> r.setAvailable(true));
-        customers.remove(customer);
-    }
+    //     if(customer.getBookedRoom().size() == 0){
+    //         connector.updateActiveCustomer(customer.getID());
+    //         customers.remove(customer);
+    //     }
+    //     System.out.println("Room checked out successfully");
+    // }
 
     public List<Room> getAvailableRoom() {
-        // return rooms.stream().filter(r -> r.isAvailable()).toList();
-        return this.rooms;
+        return roomQuery.selectAll();
     }
 
     public void addService(Service service) {
-        services.add(service);
+        serviceQuery.insert(service);
+    }
+    
+    public void removeService(Service service) {
+        serviceQuery.delete(service);
     }
 
-    public void printAllBill() {
-        customers.stream().forEach(c -> System.out.println(c.printBill()));
-        double total = customers.stream().mapToDouble(c -> c.printBill()).sum();
-        System.out.println("Total bill: " + total);
+    public void addEmployee(Employee employee) {
+        employeeQuery.insert(employee);
     }
 
-    public void caculate() {
-        double salary = employees.stream().mapToDouble(e -> e.getSalary()).sum();
-        double bills = customers.stream().mapToDouble(c -> c.getBill()).sum();
-        double profit = bills - salary;
-        System.out.println("Profit: " + profit);
-    }
+
+    
+
+    
 
 }
