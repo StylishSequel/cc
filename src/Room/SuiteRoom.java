0cebb6ca6b9d2dc1fc72ddc4027d525e6f765c2c
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-17 15:45:34 +0700

    add ctor

--- src/Room/SuiteRoom.java
+++ src/Room/SuiteRoom.java
@@ -1,28 +1,31 @@
 package Room;
 
-
-public class SuiteRoom extends Room{
+public class SuiteRoom extends Room {
     private String type = "Suite";
     private String electricDevices;
 
-    
     public SuiteRoom(int room_id, double price, int numOfBed, String electricDevices, boolean isAvailable) {
         super(room_id, price, numOfBed, isAvailable);
         this.electricDevices = electricDevices;
-        
+
     }
 
-    // public SuiteRoom(int room_id, double price, int numOfBed, String electricDevices) {
-    //     super( room_id, price, numOfBed);
-    //     this.electricDevices = electricDevices;
+    public SuiteRoom(int room_id, double price, int numOfBed, String electricDevices, boolean isAvailable, int numOfDay,
+            String check_in_date, String e_check_out_date) {
+        super(room_id, price, numOfBed, isAvailable, numOfDay, check_in_date, e_check_out_date);
+        this.electricDevices = electricDevices;
+
+    }
+
+    // public SuiteRoom(int room_id, double price, int numOfBed, String
+    // electricDevices) {
+    // super( room_id, price, numOfBed);
+    // this.electricDevices = electricDevices;
     // }
     public SuiteRoom(double price, int numOfBed, String electricDevices) {
         super(price, numOfBed);
         this.electricDevices = electricDevices;
-        this.setAvailable(true);
     }
-
-
 
     public String getElectricDevices() {
         return electricDevices;
@@ -32,18 +35,20 @@ public class SuiteRoom extends Room{
         this.electricDevices = electricDevices;
     }
 
-
-
-
     public String getType() {
         return type;
     }
+
     public String toString() {
-        return "Room Name: SuiteRoom, ID: " + this.getId() + ", Number of Beds: " + this.getNumOfBed();
-    }
-    public double calculatePrice(){
-        return this.getNumOfDay()*(this.getPrice() + (this.getNumOfBed()-1)*50) ;
+        return "Room Name: SuiteRoom, ID: " + this.getId() + ", Number of Beds: " + this.getNumOfBed()
+                + ", Electric Devices: "
+                + this.getElectricDevices() + ", Price: " + this.getPrice() + ", Check in date: "
+                + this.getCheck_in_date() + ", Number of days: " + this.getNumOfDay() + ", Expected Check out date: "
+                + this.getECheckOutDate();
     }
 
-    
+    public double calculatePrice() {
+        return this.getNumOfDay() * (this.getPrice() + (this.getNumOfBed() - 1) * 50);
+    }
+
 }
