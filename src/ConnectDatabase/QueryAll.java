52ad38ab79b9777b2aa654e598379a0983bf9f2e
thoconsungsucs <147785061+thoconsungsucs@users.noreply.github.com>
2024-05-17 09:15:33 +0700

    Update QueryAll.java

--- src/ConnectDatabase/QueryAll.java
+++ src/ConnectDatabase/QueryAll.java
@@ -1,5 +1,21 @@
 package ConnectDatabase;
 
 public class QueryAll {
+    Connector connector;
+    public QueryCustomer queryCustomer;
+    public QueryEmployee queryEmployee;
+    public QueryRoom queryRoom;
+    public QueryService queryService;
+    public QueryCustomerRoom queryCustomerRoom;
+    public QueryRoomService queryRoomService;
 
+    public QueryAll(Connector connector) {
+        this.connector = connector;
+        this.queryCustomer = new QueryCustomer(connector);
+        this.queryEmployee = new QueryEmployee(connector);
+        this.queryRoom = new QueryRoom(connector);
+        this.queryService = new QueryService(connector);
+        this.queryCustomerRoom = new QueryCustomerRoom(connector);
+        this.queryRoomService = new QueryRoomService(connector);
+    }
 }
